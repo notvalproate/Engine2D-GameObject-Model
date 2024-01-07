2dee1e52600ea88a5e046ee27f36d2930e60ca94
Ian Tauro <sushruth127@gmail.com>
2024-01-07 22:04:59 +0530

    Testing functionalities

--- main.cpp
+++ main.cpp
@@ -17,7 +17,32 @@ public:
 };
 
 int main() {
-   
+    GameObject tempCounter("Counter");
+    tempCounter.AddComponent<CounterComponent>();
+
+    for(int i = 0; i < 10; i++) {
+        tempCounter.HandleEvents();
+        tempCounter.Update();
+        tempCounter.Render();
+    }
+
+    auto tempCountersCountComponent = tempCounter.GetComponent<CounterComponent>();
+
+    std::cout << tempCountersCountComponent->count << std::endl;
+
+    auto objects = GameObject::FindObjectsByTag("Counter");
+
+    for(const auto& object : objects) {
+        std::cout << object->tag << std::endl;
+    }
+
+    auto counterObject = GameObject::FindObjectByName("Counters");
+
+    if(counterObject) {
+        std::cout << counterObject.value()->name << std::endl;
+    } else {
+        std::cout << "Could not find object" << std::endl;
+    }
 
     return 0;
 }
