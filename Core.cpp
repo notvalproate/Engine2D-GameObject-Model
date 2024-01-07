fa0f518b03bd443e6342fa866e1fba153906fb79
Ian Tauro <sushruth127@gmail.com>
2024-01-07 22:04:40 +0530

    Added game object class

--- Core.cpp
+++ Core.cpp
@@ -4,3 +4,53 @@ Component::Component(GameObject& gameObject) : gameObject(gameObject), transform
 
 Transform::Transform(GameObject& gameObject) : gameObject(gameObject) { };
 
+GameObject::GameObject() : name({}), tag({}), transform(*this) { 
+    m_GlobalGameObjectsList.push_back(this);
+};
+
+GameObject::GameObject(const std::string_view objectName) : name(objectName), tag({}), transform(*this) { 
+    m_GlobalGameObjectsList.push_back(this);
+};
+
+void GameObject::Update() const {
+    for(const auto& e : m_ComponentMap) {
+        e.second->Update();
+    }
+};
+
+void GameObject::Render() const {
+    for(const auto& e : m_ComponentMap) {
+        e.second->Render();
+    }
+};
+
+void GameObject::HandleEvents() const {
+    for(const auto& e : m_ComponentMap) {
+        e.second->HandleEvents();
+    }
+};
+
+// Much rather have this list stored in the scene object as a non-static, to avoid searching through unnecessary objects that arent in the scene
+std::vector<const GameObject*> GameObject::m_GlobalGameObjectsList{};
+
+std::optional<const GameObject*> GameObject::FindObjectByName(const std::string_view searchName) {
+    for(const auto& object : m_GlobalGameObjectsList) {
+        if(object->name == searchName) {
+            return object;
+        }
+    }
+
+    return {};
+}
+
+std::vector<const GameObject*> GameObject::FindObjectsByTag(const std::string_view searchTag) {
+    std::vector<const GameObject*> objects{};
+
+    for(const auto& object : m_GlobalGameObjectsList) {
+        if(object->tag == searchTag) {
+            objects.push_back(object);
+        }
+    }
+
+    return objects;
+}
--- Core.hpp
+++ Core.hpp
@@ -51,3 +51,66 @@ private:
     friend class GameObject;
 };
 
+class GameObject final {
+public:
+    GameObject();
+    GameObject(const std::string_view objectName);
+
+    ~GameObject() = default;
+
+    void Update() const;
+    void Render() const;
+    void HandleEvents() const;
+
+    static std::optional<const GameObject*> FindObjectByName(const std::string_view searchName);
+    static std::vector<const GameObject*> FindObjectsByTag(const std::string_view searchTag);
+
+    template<typename Last>
+    void AddComponent() {
+        AssertParametersAreDerived<Last>();
+
+        auto [it, success] = m_ComponentMap.try_emplace(typeid(Last), std::unique_ptr<Last>(new Last(*this)));
+
+        if(success) {
+            m_ComponentMap[typeid(Last)]->Start();
+        }
+    }
+
+    template<typename First, typename Second, typename... Args>
+    void AddComponent() {
+        AssertParametersAreDerived<First, Second, Args...>();
+
+        auto [it, success] = m_ComponentMap.try_emplace(typeid(First), std::unique_ptr<First>(new First(*this)));
+
+        if(success) {
+            m_ComponentMap[typeid(First)]->Start();
+        }
+            
+        AddComponent<Second, Args...>();
+    }
+
+    template<typename T>
+    T* GetComponent() const {
+        AssertParametersAreDerived<T>();
+
+        auto it = m_ComponentMap.find(typeid(T));
+
+        return (it != m_ComponentMap.end()) ? dynamic_cast<T*>(it->second.get()) : nullptr;
+    }
+
+    std::string name;
+    std::string tag;
+    Transform transform;
+
+private:
+    std::unordered_map<std::type_index, std::unique_ptr<Component>> m_ComponentMap{};
+    static std::vector<const GameObject*> m_GlobalGameObjectsList;
+    
+    template<typename... Args>
+    static void AssertParametersAreDerived() {
+        static_assert(
+            std::conjunction<std::is_base_of<Component, Args>...>::value, 
+            "Custom Component provided not derived from Component Class"
+        );
+    }
+};
