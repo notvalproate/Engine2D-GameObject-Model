f55e400d7b3aae221454339299d05e777f1db9af
Ian Tauro <sushruth127@gmail.com>
2024-01-13 11:21:19 +0530

    changed . to ->

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ public:
         std::cout << "Setup Started" << std::endl;
 
         auto Parent = CreateGameObject("Parent");
-        Parent.AddComponent<PositionDebug>();
-        Parent.tag = "Parent";
+        Parent->AddComponent<PositionDebug>();
+        Parent->tag = "Parent";
 
         auto Child = CreateGameObject("Child");
-        Child.AddComponent<PositionDebug, LocalPositionDebug>();
-        Child.tag = "Child";
+        Child->AddComponent<PositionDebug, LocalPositionDebug>();
+        Child->tag = "Child";
 
-        Child.transform.SetParent(&Parent.transform);
+        Child->transform.SetParent(&Parent->transform);
 
-        auto childTransform = Parent.transform.Find("Child");
+        auto childTransform = Parent->transform.Find("Child");
 
         if(childTransform) {
             std::cout << "Found " << childTransform->name << std::endl;
@@ -27,14 +27,14 @@ public:
 
         std::cout << std::boolalpha;
 
-        std::cout << Child.transform.IsChildOf(&Parent.transform) << std::endl;
+        std::cout << Child->transform.IsChildOf(&Parent->transform) << std::endl;
 
         // Parent.transform.DetachChildren();
 
-        std::cout << Parent.transform.IsChildOf(&Child.transform) << std::endl;
+        std::cout << Parent->transform.IsChildOf(&Child->transform) << std::endl;
 
-        Child.transform.Translate(Vector2D(1.0, 1.0));
-        Parent.transform.Translate(Vector2D(5.0, 5.0));
+        Child->transform.Translate(Vector2D(1.0, 1.0));
+        Parent->transform.Translate(Vector2D(5.0, 5.0));
 
         std::cout << "Setup Complete" << std::endl;
     }
